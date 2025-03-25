6bf1ec852109abb1672ce8cafd1e8e2152048a41
valentin <valentinmorice1@gmail.com>
2025-03-25 11:03:37 +0100

    add visual clue for disabled viewer

--- resources/views/components/json-toggle.blade.php
+++ resources/views/components/json-toggle.blade.php
@@ -8,7 +8,9 @@
     <div class="container flex justify-between">
         <div style="display: flex; font-size: 0.875rem; line-height: 1.25rem;">
             <div class="control"
+                 x-data="{ id: $id('display-toggle') }"
                  x-on:click="!jsonError && (display = 'viewer')"
+                 x-bind:style="jsonError && { opacity: 0.5 }"
                  x-bind:class="{
                     'active': display === 'viewer',
                  }"
